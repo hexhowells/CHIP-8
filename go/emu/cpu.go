e207167e99e43f69a7b9f696baad3911beb1af2d
hexhowells <58524460+hexhowells@users.noreply.github.com>
2026-05-31 20:55:03 +0100

    create basic cpu structure

---
+++ go/emu/cpu.go
@@ -0,0 +1,174 @@
+type CPU struct {
+	Pc uint8
+	I uint16
+	Vc [16]uint8
+	Stack [16]uint16
+	Memory [4096]uint8
+	opcode uint8
+}
+
+
+func NewCPU() *CPU {
+	cpu := CPU()
+	opcode := 0x00
+
+	lookup = []{}
+}
+
+func (cpu *CPU) Clock() {
+	cpu.opcode := cpu.Read(cpu.Pc)
+
+	inst := cpu.lookup[cpu.opcode]
+}
+
+func (cpu * CPU) _0NNN {
+
+}
+
+func (cpu * CPU) _00E0 {
+	
+}
+
+func (cpu * CPU) _1NNN {
+	
+}
+
+func (cpu * CPU) _00EE {
+	
+}
+
+func (cpu * CPU) _2NNN {
+	
+}
+
+func (cpu * CPU) _0NNN {
+	
+}
+
+func (cpu * CPU) _3XNN {
+	
+}
+
+func (cpu * CPU) _4XNN {
+	
+}
+
+func (cpu * CPU) _5XY0 {
+	
+}
+
+func (cpu * CPU) _9XY0 {
+	
+}
+
+func (cpu * CPU) _3XNN {
+	
+}
+
+func (cpu * CPU) _5XY0 {
+	
+}
+
+func (cpu * CPU) _6XNN {
+	
+}
+
+func (cpu * CPU) _7XNN {
+	
+}
+
+func (cpu * CPU) _8XY0 {
+	
+}
+
+func (cpu * CPU) _8XY1 {
+	
+}
+
+func (cpu * CPU) _8XY2 {
+	
+}
+
+func (cpu * CPU) _8XY3 {
+	
+}
+
+func (cpu * CPU) _8XY4 {
+	
+}
+
+func (cpu * CPU) _8XY5 {
+	
+}
+
+func (cpu * CPU) _8XY7 {
+	
+}
+
+func (cpu * CPU) _8XY6 {
+	
+}
+
+func (cpu * CPU) _8XYE {
+	
+}
+
+func (cpu * CPU) _ANNN {
+	
+}
+
+func (cpu * CPU) _BNNN {
+	
+}
+
+func (cpu * CPU) _CXNN {
+	
+}
+
+func (cpu * CPU) _DXYN {
+	
+}
+
+func (cpu * CPU) _EX9E {
+	
+}
+
+func (cpu * CPU) _EXA1 {
+	
+}
+
+func (cpu * CPU) _FX07 {
+	
+}
+
+func (cpu * CPU) _FX15 {
+	
+}
+
+func (cpu * CPU) _FX18 {
+	
+}
+
+func (cpu * CPU) _FX1E {
+	
+}
+
+func (cpu * CPU) _FX0A {
+	
+}
+
+func (cpu * CPU) _FX29 {
+	
+}
+
+func (cpu * CPU) _FX33 {
+	
+}
+
+func (cpu * CPU) _FX55 {
+	
+}
+
+func (cpu * CPU) _FX65 {
+	
+}
