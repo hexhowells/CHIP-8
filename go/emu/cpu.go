cfc322b227d215c17a71b42417d7034f6cb3fc29
hexhowells <58524460+hexhowells@users.noreply.github.com>
2026-06-04 23:06:22 +0100

    implement various instructions

--- go/emu/cpu.go
+++ go/emu/cpu.go
@@ -10,6 +10,7 @@ type CPU struct {
 	I uint16  // index register
 	Vc [16]uint8  // variable registers (16 8-bit)
 	Stack [16]uint16
+	sp uint16  // stack pointer
 	Memory [4096]uint8  // 4kB memory space
 	Screen [32][64]uint8
 	opcode uint8
@@ -69,10 +70,10 @@ depends on the specific instruction being run.
 1010 0010 0110 1011
 
 OP - first nibble, primary opcode group (1010)
-VC - second nibble, usually looking up one of the registers (V0 - VF) (0010)
-Y - third nibble, usually looking up a second register (0110)
+VC (VX/X) - second nibble, usually looking up one of the registers (V0 - VF) (0010)
+Y  (VY) - third nibble, usually looking up a second register (0110)
 N - fourth nibble, a 4-bit immediate value (1011)
-KK - second byte (last two nibbles), a 8-bit immediate value (0110 1011)
+KK (NN) - second byte (last two nibbles), a 8-bit immediate value (0110 1011)
 NNN - last three nibbles, a 12-bit immediate memory address (0010 0110 1011)
 
 */
@@ -109,43 +110,70 @@ func (cpu *CPU) _00E0() {
 }
 
 
+// jump
 func (cpu *CPU) _1NNN() {
-	
+	cpu.Pc = cpu.oprand
 }
 
 
+// return from subroutine
 func (cpu *CPU) _00EE() {
-	
+	cpu.Pc = cpu.stack[cpu.sp-1]
+	cpu.stack[cpu.sp] = 0x0000
+	cpu.sp -= 1
 }
 
 
+// call subroutine
 func (cpu *CPU) _2NNN() {
-	
+	cpu.stack[cpu.sp] = cp.Pc
+	cpu.sp += 1
+	cpu.Pc = cpu.oprand
 }
 
 
+// skip one instruction if VX == NN
 func (cpu *CPU) _3XNN() {
-	
+	reg := uint8((cpu.oprand & 0x0F00) > 8)
+	if cpu.Vc[reg] == uint8(cpu.oprand & 0x00FF) {
+		cpu.Pc += 2
+	}
 }
 
 
+// skip one instruction if VX != NN
 func (cpu *CPU) _4XNN() {
-	
+	reg := uint8((cpu.oprand & 0x0F00) > 8)
+	if cpu.Vc[reg] != uint8(cpu.oprand & 0x00FF) {
+		cpu.Pc += 2
+	}
 }
 
 
+// skip one instruction if VX == VY
 func (cpu *CPU) _5XY0() {
-	
+	vx := uint8((cpu.oprand & 0x0F00) > 8)
+	vy := uint8((cpu.oprand & 0x00F0) > 4)
+	if cpu.Vc[vx] == cpu.Vc[vx] {
+		cpu.Pc += 2
+	}
 }
 
 
+// skip one instruction if VX != VY
 func (cpu *CPU) _9XY0() {
-	
+	vx := uint8((cpu.oprand & 0x0F00) > 8)
+	vy := uint8((cpu.oprand & 0x00F0) > 4)
+	if cpu.Vc[vx] != cpu.Vc[vx] {
+		cpu.Pc += 2
+	}
 }
 
 
+// set
 func (cpu *CPU) _6XNN() {
-	
+	reg := uint8((cpu.oprand & 0x0F00) > 8)
+	cpu.Vc[reg] = uint8(cpu.oprand & 0x00FF)
 }
 
 
